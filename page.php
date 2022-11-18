f6cc5faf5f629d7a81fdc4bbb2b23ce50c13a00a
erfan :) <116520593+erfanaghababaei@users.noreply.github.com>
2022-11-18 18:19:00 +0330

    Add files via upload

---
+++ page.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang= "en">
+    <head>
+       <meta charset="UTF-8">
+          <meta name="description" content="this is a weblog page">
+             <meta name="keyword" content="Gitmag">
+                <meta name="author" content="Erfan Aghababaei">
+             <title>Gitmag weblog</title>
+          <link rel="stylesheet" href="page.css">
+    </head>
+
+    <body>
+        <main>
+            <header>
+                <h1>Gitmag website</h1>
+                <div id= "logo" >
+                    <img src="" alt="">
+                </div>
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Galery</a></li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
+                <form action="#" method="GET">
+                    <input type="text" name="search" placeholder="search your word">
+                    <input type="submit" value="Search">
+                </form>
+            </nav>
+            <section id="content">
+                <aside>
+                    <div class="aside-box">
+                        <h2>Top post</h2>
+                        <ul>
+                            <li><a href="#">This is post 1</a></li>
+                            <li><a href="#">This is post 2</a></li>
+                            <li><a href="#">This is post 3</a></li>
+                            <li><a href="#">This is post 4</a></li>
+                            <li><a href="#">This is post 5</a></li>
+                            <li><a href="#">This is post 6</a></li>
+                            <li><a href="#">This is post 7</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="aside-box">
+                        <h2>Last post</h2>
+                        <ul>
+                            <li><a href="#">This is post 1</a></li>
+                            <li><a href="#">This is post 2</a></li>
+                            <li><a href="#">This is post 3</a></li>
+                            <li><a href="#">This is post 4</a></li>
+                            <li><a href="#">This is post 5</a></li>
+                            <li><a href="#">This is post 6</a></li>
+                            <li><a href="#">This is post 7</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="aside-box">
+                        <h2>Best post</h2>
+                        <ul>
+                            <li><a href="#">This is post 1</a></li>
+                            <li><a href="#">This is post 2</a></li>
+                            <li><a href="#">This is post 3</a></li>
+                            <li><a href="#">This is post 4</a></li>
+                            <li><a href="#">This is post 5</a></li>
+                            <li><a href="#">This is post 6</a></li>
+                            <li><a href="#">This is post 7</a></li>
+                        </ul>
+                    </div>
+                </aside>
+                <div id ="articles">
+                    <article>
+                        <div class="caption">
+                            <h3>This is a post</h3>
+                            <ul>
+                                <li>Date:<span>2020-05-15</span></li>
+                                <li>Views:<span>1526 view</span></li>
+                            </ul>
+                            <p>
+                                In publishing and grafic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document of a typeface without relying on meaningful content. lorem ipsum may be used as a placeholder before final copy is available.
+                            </p>
+                            <a href="#"> More...</a>
+                        </div>
+                        <div class="image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="clearfix"></div>
+                    </article>
+                    <article>
+                        <div class="caption">
+                            <h3>This is a post</h3>
+                            <ul>
+                                <li>Date:<span>2020-05-15</span></li>
+                                <li>Views:<span>1526 view</span></li>
+                            </ul>
+                            <p>
+                                In publishing and grafic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document of a typeface without relying on meaningful content. lorem ipsum may be used as a placeholder before final copy is available.
+                            </p>
+                            <a href="#"> More...</a>
+                        </div>
+                        <div class="image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="clearfix"></div>
+                    </article>
+                    <article>
+                        <div class="caption">
+                            <h3>This is a post</h3>
+                            <ul>
+                                <li>Date:<span>2020-05-15</span></li>
+                                <li>Views:<span>1526 view</span></li>
+                            </ul>
+                            <p>
+                                In publishing and grafic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document of a typeface without relying on meaningful content. lorem ipsum may be used as a placeholder before final copy is available.
+                            </p>
+                            <a href="#"> More...</a>
+                        </div>
+                        <div class="image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="clearfix"></div>
+                    </article>
+                    <article>
+                        <div class="caption">
+                            <h3>This is a post</h3>
+                            <ul>
+                                <li>Date:<span>2020-05-15</span></li>
+                                <li>Views:<span>1526 view</span></li>
+                            </ul>
+                            <p>
+                                In publishing and grafic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document of a typeface without relying on meaningful content. lorem ipsum may be used as a placeholder before final copy is available.
+                            </p>
+                            <a href="#"> More...</a>
+                        </div>
+                        <div class="image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="clearfix"></div>
+                    </article>
+                </div>
+                <div class="clearfix"></div>
+            </section>
+            <footer>
+                <p>Copyright <a href="#">Gitmag</a></p>
+            </footer>
+        </main>
+    </body>
+</html>
